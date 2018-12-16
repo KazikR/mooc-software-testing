ec737a793e231bb70903c1da01e871c81390a4b2
Kazik <kazikr@gmail.com>
2018-12-16 21:02:27 +0100

    caesar fix + tests

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,20 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    void abc3() {
+        CaesarShiftCipher a = new CaesarShiftCipher();
+        Assertions.assertEquals("def", a.CaesarShiftCipher("abc", 3));
+    }
+
+    @Test
+    void xyz3() {
+        CaesarShiftCipher a = new CaesarShiftCipher();
+        Assertions.assertEquals("abc", a.CaesarShiftCipher("xyz", 3));
+    }
 
 }
